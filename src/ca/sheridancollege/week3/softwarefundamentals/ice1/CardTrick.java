a33ab7c8097175736591f14d422d1aa8da31dd3b
saeedbas-bassam <saeedbas@sheridancollege.ca>
2019-01-28 14:00:27 -0500

    Added author name

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
- * @author
+ * @author Bassam Saeed
  */
 public class CardTrick {
     

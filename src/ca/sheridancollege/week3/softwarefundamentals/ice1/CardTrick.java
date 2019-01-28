9d0565f4e3ddfc5dc42778397e60d94b96ad153f
Owen Ross <rossow@sheridancollege.ca>
2019-01-28 14:37:35 -0500

    Finished code: Added user input.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,10 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* Name: Owen Ross
+* Student ID: 991484122
+*/
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -17,13 +16,26 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        int randomNum;
+        
+        Scanner in= new Scanner(System.in);
+        
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
+            magicHand[i]=new Card();
+            randomNum = 1 + (int)(Math.random() * 13);
+            magicHand[i].setValue(randomNum);
+            magicHand[i].setSuit("Clubs");
+            
+            System.out.println("Card no " + (i + 1) + ". is: " + magicHand[i].getValue() + " of " + magicHand[i].getSuit());
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
+        System.out.println("Please pick a card between 1 and 7");
+        int userPick = in.nextInt();
+        
+        System.out.println("You picked the " + magicHand[userPick - 1].getValue() + " of " +  magicHand[userPick - 1].getSuit());
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here

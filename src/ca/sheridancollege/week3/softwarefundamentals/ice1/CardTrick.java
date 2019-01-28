3d842af059cbe279a0de5da66c4371bba108da6c
michelletran <michelletran@10.16.49.57>
2019-01-28 14:16:03 -0500

    Code changed - remove switch add method to set value and suit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -19,62 +19,11 @@ public class CardTrick {
     
     public static void main(String[] args){
         Card[] magicHand = new Card[7];
-        int r =0;
         for (int i=0; i<magicHand.length; i++){
             Card c = new Card();
-            c.setValue(r);
-            c.getValue();
-     public String selectRandomCardRank() {
-        String cardRank;
-        int r = 1 + gen.nextInt(13);
-        switch (r) {
-        case 1:
-            cardRank = "Ace";
-            break;
-        case 2:
-            cardRank = "2";
-            break;
-        case 3:
-            cardRank = "3";
-            break;
-        case 4:
-            cardRank = "4";
-            break;
-        case 5:
-            cardRank = "5";
-            break;          
-        case 6:
-            cardRank = "6";
-            break;
-        case 7:
-            cardRank = "7";
-            break;
-        case 8:
-            cardRank = "8";
-            break;
-        case 9:
-            cardRank = "9";
-            break;
-        case 10:
-            cardRank = "10";
-            break;
-        case 11:
-            cardRank = "Jack";
-            break;
-        case 12:
-            cardRank = "Queen";
-            break;          
-        default:
-            cardRank = "King";
-            break;
-        }
-        return cardRank;
-    }
-     
-//(insert call to random number generator here)
-            c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(Math.random()*14);
+            c.setSuit(Card.SUITS[Math.random()*4]]);
         
-        }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here

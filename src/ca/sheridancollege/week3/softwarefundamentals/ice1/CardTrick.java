b14ad061ffaa38c451fbfb9d6ead9d117fa6bfb4
mohmahmo <46901780+mohmahmo@users.noreply.github.com>
2019-01-28 21:26:19 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -31,6 +31,9 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        
+        
+        // We are not coding
     }
     
 }

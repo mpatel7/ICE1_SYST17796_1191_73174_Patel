603ca892628a2c9eeaf863ada04f4bd7108baacb
adhitamapatrick <46901719+adhitamapatrick@users.noreply.github.com>
2019-02-04 12:29:20 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -23,6 +23,7 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        //comment 4heart
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here

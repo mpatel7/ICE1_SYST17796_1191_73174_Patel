d90830aff99b9b4905493f2261dd80ae75050673
WeiyeChen <chenweiy@sheridancollege.ca>
2019-02-04 12:12:42 -0500

    new repo for Student code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,33 +4,55 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * Student：Weiye Chen
+ * Number：991527063
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
-        Card luckyCard = new Card();
-        luckyCard.number = 4;
-        luckyCard.suit = heart;
-        
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            magicHand[i] = c;
+            
+            c.setValue((int)(Math.random()*13)+1);
+            
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
         }
+       
         
         //insert code to ask the user for Card value and suit, create their card
+        Card luckyCard = new Card();
+
+	luckyCard.setValue(3);
+
+	luckyCard.setSuit(Card.SUITS[1]);
+        
+        System.out.println("Your luckycard is "+luckyCard.getValue()+" of "+luckyCard.getSuit());
         // and search magicHand here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the card value:");
+        
+        int value = input.nextInt();
+        
+        System.out.print("Enter the suit of the card:");
+        
+        String suit = input.nextLine();
         //Then report the result here
+       
+        
     }
     
 }
